a20f0e2f2a3eecb51a7fdc74c0a139273938eab6
emilio-quintana-dev <emilioquintana90@gmail.com>
2020-08-29 13:36:26 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>Pomodoro Clock</h1>
+      </main>
+    );
+  }
+}
+
+export default App;
