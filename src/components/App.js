75ec931a08d46c2bdcfa495533221fcea6d67f8a
emilio-quintana-dev <emilioquintana90@gmail.com>
2020-08-29 14:22:25 -0500

    added basic counter functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,49 @@
 import React from "react";
 import "../App.css";
+import BreakInterval from "./BreakInterval";
+import SessionLength from "./SessionLenght";
+import Timer from "./Timer";
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25,
+      timerMinute: 25,
+    };
+  }
+
+  onIncreaseBreakLength = () => {
+    this.setState((prevState) => {
+      return {
+        breakLength: prevState.breakLength + 1,
+      };
+    });
+  };
+
+  onDecreaseBreakLength = () => {
+    this.setState((prevState) => {
+      return {
+        breakLength: prevState.breakLength - 1,
+      };
+    });
+  };
+
   render() {
     return (
       <main>
-        <h1>Pomodoro Clock</h1>
+        <h2>Pomodoro Clock</h2>
+        <section className="interval-length-container">
+          <BreakInterval
+            breakInterval={this.state.breakLength}
+            increaseBreak={this.onIncreaseBreakLength}
+            decreaseBreak={this.onDecreaseBreakLength}
+          />
+          <SessionLength sessionLength={this.state.sessionLength} />
+        </section>
+        <Timer timerMinute={this.state.timerMinute} />
       </main>
     );
   }
