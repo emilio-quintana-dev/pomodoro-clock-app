4bf5a74033c1c45ab41da3c600255f3b49f2ea41
emilio-quintana-dev <emilioquintana90@gmail.com>
2020-08-31 20:17:34 -0500

    added soundFX to click and made some styling fixes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
-const Button = styled.section`
-  background: #379683;
+const Button = styled.button`
+  background: #05386b;
   border-radius: 3px;
   color: #fff;
   margin: 0.5em 0.5em;
